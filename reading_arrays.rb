451acf01864423471b4fd7b96cf4c0c7caa80679
likhitaaluru <likhitaaluru@gmail.com>
2018-08-08 15:34:26 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -20,11 +20,11 @@ def last_student_by_index
 end
 
 def first_student_by_method
-  # Write a solution that returns the first student using the built in .first method
+STUDENT_NAMES.first
 end
 
 def last_student_by_method
-  # Write a solution that returns the first student using the built in .last method
+STUDENT_NAMES.last
 end
 
 def first_second_and_third_students
